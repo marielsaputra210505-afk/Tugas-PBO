c0ccd31c8923dac56e975cb01001fcdf2432b6fd
Muhammad Ariel Saputra <marielsaputra210505@gmail.com>
2026-04-12 23:57:21 +0800

    Create  BacaString.java

---
+++ Tugas 2/Tugas 2_SourceCode/BacaString.java
@@ -0,0 +1,14 @@
+//Nama  : Muhammad Ariel Saputra
+//Nim   : 13020240270
+//Kelas : A3
+
+import java.io.*;
+
+public class BacaString {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Masukkan string: ");
+        String str = br.readLine();
+        System.out.println("Hasil: " + str);
+    }
+}

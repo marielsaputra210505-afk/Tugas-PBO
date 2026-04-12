d772401461538319a789e6063679b8de5f15121a
Muhammad Ariel Saputra <marielsaputra210505@gmail.com>
2026-04-12 23:59:44 +0800

    Create  ForEver.java

---
+++ Tugas 2/Tugas 2_SourceCode/ForEver.java
@@ -0,0 +1,12 @@
+//Nama  : Muhammad Ariel Saputra
+//Nim   : 13020240270
+//Kelas : A3
+
+public class ForEver {
+    public static void main(String[] args) throws Exception {
+        while (true) {
+            System.out.println("Loop berjalan...");
+            Thread.sleep(1000);
+        }
+    }
+}

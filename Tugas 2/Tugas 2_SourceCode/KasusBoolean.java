d914324a8d1b8cfe10a5f8876bd45f645c4b4f26
Muhammad Ariel Saputra <marielsaputra210505@gmail.com>
2026-04-13 00:04:54 +0800

    Create KasusBoolean.java

---
+++ Tugas 2/Tugas 2_SourceCode/KasusBoolean.java
@@ -0,0 +1,21 @@
+//Nama  : Muhammad Ariel Saputra
+//Nim   : 13020240270
+//Kelas : A3
+
+public class KasusBoolean {
+    public static void main(String[] args) {
+        boolean bool = true;
+
+        if (bool) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+
+        if (!bool) {
+            System.out.println("salah");
+        } else {
+            System.out.println("benar");
+        }
+    }
+}
